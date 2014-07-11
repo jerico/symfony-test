d1f929b3d753f380a36337d755e916cff6ed431a
Jerico Aragon <jerico@gmail.com>
2014-07-11 14:19:42 +0800

    Slugify
    Smooth sailing. Mas kaya ko na handle mga errors.

--- app/DoctrineMigrations/Version20140711135510.php
+++ app/DoctrineMigrations/Version20140711135510.php
@@ -8,15 +8,14 @@ use Doctrine\DBAL\Schema\Schema;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-class Version20140709142725 extends AbstractMigration
+class Version20140711135510 extends AbstractMigration
 {
     public function up(Schema $schema)
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != "mysql", "Migration can only be executed safely on 'mysql'.");
         
-        $this->addSql("CREATE TABLE comment (id INT AUTO_INCREMENT NOT NULL, blog_id INT DEFAULT NULL, user VARCHAR(255) NOT NULL, comment LONGTEXT NOT NULL, approved TINYINT(1) NOT NULL, created DATETIME NOT NULL, updated DATETIME NOT NULL, INDEX IDX_9474526CDAE07E97 (blog_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB");
-        $this->addSql("ALTER TABLE comment ADD CONSTRAINT FK_9474526CDAE07E97 FOREIGN KEY (blog_id) REFERENCES blog (id)");
+        $this->addSql("ALTER TABLE blog ADD slug VARCHAR(255) NOT NULL");
     }
 
     public function down(Schema $schema)
@@ -24,6 +23,6 @@ class Version20140709142725 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != "mysql", "Migration can only be executed safely on 'mysql'.");
         
-        $this->addSql("DROP TABLE comment");
+        $this->addSql("ALTER TABLE blog DROP slug");
     }
 }
